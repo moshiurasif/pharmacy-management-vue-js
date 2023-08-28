e5f6f0df1c80fff2cb60fa9a3848ff38499d3ed2
Moshiur Asifm <asifmd2212@gmail.com>
2023-08-28 18:53:39 +0600

    adding login form by tailwind css daizy ui

---
+++ src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="hero min-h-screen bg-base-200">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <div class="text-center lg:text-left">
+      <h1 class="text-5xl font-bold">Login now!</h1>
+      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    </div>
+    <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <div class="card-body">
+       <form action="" @submit.prevent="handleSubmit">
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" class="input input-bordered" v-model="formData.email"/>
+        </div>
+        <div class="form-control">
+          <label class="label">
+            <span class="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" class="input input-bordered" v-model="formData.password"/>
+          <label class="label">
+            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div class="form-control mt-6">
+          <button class="btn btn-primary" type="submit">Login</button>
+        </div>
+       </form>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+    export default {
+        data:()=>({
+           formData:{
+            email: "",
+            password: ""
+           }
+        }),
+        methods:{
+            handleSubmit(){
+                console.log(this.formData);
+                if (!this.formData.email) {
+                    // alert("Email Field is Required")
+                    this.$toast.show(`Email Field is Required`,{
+                        position:"top-right"
+                    });
+                }
+                if (!this.formData.password && this.formData.password.length < 8) {
+                    // alert("password at least 8 characters")
+                    this.$toast.show(`password field is required`);
+                    this.$toast.show(`password at least 8 characters`);
+                    
+                }
+                this.formData = {}
+            }
+        }
+        
+    }
+
+    
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Toaster from "@meforma/vue-toaster";
 
-createApp(App).mount('#app')
+createApp(App).use(Toaster).mount("#app");
